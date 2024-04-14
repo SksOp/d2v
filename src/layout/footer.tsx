669c18a10c2c77167bce0220e63ef56dd0cc6a50
SksOp <shubhaman47@gmail.com>
2024-04-14 14:48:07 +0530

    fix : layout footer ui

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -7,11 +7,27 @@ const INFO_ICON_SVG =
 
 export function Footer() {
   return (
-    <Stack direction="row" alignItems="center">
-      <img src={INFO_ICON_SVG} alt="Info" />
-      <Typography variant="body2">{UICONFIG.footerline}</Typography>
+    <Stack direction="row" alignItems="center" width="100%" px={2.2} gap={1}>
+      <Box
+        component="img"
+        width={16}
+        height={16}
+        src={INFO_ICON_SVG}
+        alt="Info"
+        bgcolor="black"
+        borderRadius={999}
+      />
+      <Typography variant="body2" fontSize="0.82rem">
+        {UICONFIG.footerline}
+      </Typography>
       <Box sx={{ flexGrow: 1 }} />
-      <img src={UICONFIG.footerlineIcon} alt="Logo" />
+      {/* <img src={UICONFIG.footerlineIcon} alt="Logo" /> */}
+      <Box
+        component="img"
+        src={UICONFIG.footerlineIcon}
+        alt="Logo"
+        width={54}
+      />
     </Stack>
   );
 }
