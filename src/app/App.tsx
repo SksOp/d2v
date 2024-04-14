21f90bf7e4a6de06d37fe38441578ba60f551195
shubhamCodeEngine <shubham.k@code-engine.net>
2024-04-14 11:23:50 +0530

    initiated with mui and vite

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Button variant="contained">Hello I am button</Button>
+    </>
+  );
+}
+
+export default App;
